5e750d51fc2be09a823806ad025cf1bd1101bdf7
Toan Nguyen <toan.nguyen.doan@gmail.com>
2021-12-01 15:03:30 +0100

    :

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-var commiter_name = whoami
-var today_date = date +"%d-%m-%y-%HH-%MM-%SS"
+var commiter_name = 'whoami'
+var today_date = 'date +"%d-%m-%y-%HH-%MM-%SS"'
 echo $commiter_name
 echo $today_date
 
