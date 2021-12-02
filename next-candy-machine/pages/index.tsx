f887aebce91b519d4b731cc9467e08e3ce3b37dc
Toan Nguyen <toan.nguyen.doan@gmail.com>
2021-12-02 22:14:58 +0100

    devobeast : 02-12-21-22H-14M-58S

--- next-candy-machine/pages/index.tsx
+++ next-candy-machine/pages/index.tsx
@@ -86,7 +86,7 @@ export default function Home() {
 
       <nav className="navbar sticky-top navbar-expand-lg navbar-dark">
       <div className="container-fluid">
-
+    
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
