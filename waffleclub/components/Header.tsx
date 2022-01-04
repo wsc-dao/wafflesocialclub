a99788a77856b2155fcb652b6ca7cfaf6e0d742c
Arne Herbots <arne.herbots@unikoo.be>
2022-01-04 18:12:45 +0100

    update header

--- waffleclub/components/Header.tsx
+++ waffleclub/components/Header.tsx
@@ -31,18 +31,19 @@ const CustomHeader = styled.header<{ scrolled: Boolean }>`
 
   .balance {
     max-width: 300px;
-    position: absolute;
-    right: 0;
-    top: 50%;
-    bottom: 50%;
-    transform: translateY(-50%);
     display: flex;
+    flex-direction: column;
     align-items: center;
     color: #ff9900;
 
-    p {
-      margin-right: 15px;
+    .wallet-adapter-button-trigger {
+      background: #f5d8ad;
+      color: #B8202E;
+    }
 
+    p {
+      margin-top: 15px;
+      margin-bottom: 0;
       //noinspection CssInvalidPropertyValue
       .currency {
         -webkit-background-clip: text;
@@ -77,22 +78,24 @@ export const Header = ({connected, balance}: HeaderProps) => {
   return <CustomHeader scrolled={navbar}>
     <nav>
       <ul>
-        <li><Link href={'#about'}>About</Link></li>
-        <li><Link href={'/roadmap'}>Roadmap</Link></li>
-        <li><Link href={'#team'}>Waffle Team</Link></li>
         <li><Link href={'/'}><img src="/waffledao_rebrand_logo.png" alt="logo"/></Link></li>
-        <li><Link href={'#mint'}>Mint</Link></li>
+        <li><Link href={'/roadmap'}>Roadmap</Link></li>
+        <li><Link href={'/mint'}>Mint</Link></li>
+        <li><Link href={'#about'}>About</Link></li>
+        <li><Link href={'#team'}>Waffle Team</Link></li>
         <li><Link href={'#mynft'}>Collection</Link></li>
         <li><Link href={'#faq'}>FAQ</Link></li>
+        <li>
+          <div className="balance">
+            <WalletMultiButton/>
+            {connected && (<p className="">Balance:
+                <span className="value">{' '}{balance.toFixed(2)}</span>
+                <span className="currency">{' '}SOL</span>
+              </p>
+            )}
+          </div>
+        </li>
       </ul>
     </nav>
-    <div className="balance">
-      {connected && (<p className="">Balance:
-          <span className="value">{' '}{balance.toFixed(2)}</span>
-          <span className="currency">{' '}SOL</span>
-        </p>
-      )}
-      <WalletMultiButton/>
-    </div>
   </CustomHeader>
 }
