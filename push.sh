9dd15db99d7958cb38426e71b60f6544055f010f
Toan Nguyen <toan.nguyen.doan@gmail.com>
2022-01-12 23:22:17 +0100

    devobeast : update push.sh for comitting message instead of date :p

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ echo $commiter_name
 echo $today_date
 
 git add *
-git commit -m "$commiter_name : $today_date $2"
+git commit -m "$commiter_name : $today_date $2 $3"
 git push
 
 # git checkout -b <newbranch>
