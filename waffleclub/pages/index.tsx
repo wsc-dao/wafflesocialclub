1c42baaa0cb6ff8e32b0ac2f06ae11907247dfa5
Arne Herbots <arne.herbots@unikoo.be>
2022-01-19 11:32:35 +0100

    fix backgound coming soon

--- waffleclub/pages/index.tsx
+++ waffleclub/pages/index.tsx
@@ -1,6 +1,6 @@
 import {Section} from "../components/Section";
 
-const App = () => <Section style={{width: "100%", height: '100vh', background:"url('/banner.png') no-repeat cover", backgroundSize:'cover'}}>
+const App = () => <Section style={{width: "100%", height: '100vh',  background:"url('/banner.png') no-repeat cover",backgroundSize:'cover', backgroundPosition:'center'}}>
   <h1 style={{textAlign: "center",}}>Baking Waffles</h1>
 </Section>
 
